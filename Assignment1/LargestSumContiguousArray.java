09e6b6633b5a0c3a99e5c45c3356af3450f833e1
Ganesh Karicharla <ganeshkaricherla@gmail.com>
2021-02-16 08:39:29 +0530

    Assignment1

--- Assignment1/LargestSumContiguousArray.java
+++ Assignment1/LargestSumContiguousArray.java
@@ -1,1 +1,24 @@
-# Kadanes Algo
+class LargestSumContiguousArray
+{
+    public static void main(String[] args)
+    {
+        int [] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+
+        int maxTillNow = Integer.MIN_VALUE;
+        int maxEnd = 0;
+        for(int i =0; i <arr.length; i=i+1)
+        {
+            System.out.println("T:"+maxTillNow + "S:"+maxEnd);
+           maxEnd = maxEnd + arr[i];
+           if(maxTillNow < maxEnd)
+           {
+               maxTillNow = maxEnd;
+           }
+           if(maxEnd < 0)
+           {
+               maxEnd = 0;
+           }
+        }
+        System.out.print(maxTillNow);
+    }
+}
