32cdcb92422eb86be0f825e7fc62dc1043abc83c
Ganesh Karicharla <ganeshkaricherla@gmail.com>
2021-02-20 21:30:05 +0530

    Assignment 5

---
+++ Assignment5/BTNode.java
@@ -0,0 +1,15 @@
+public class BTNode{
+    int data;
+    BTNode left;
+    BTNode right;
+    BTNode(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+    BTNode(){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
